4b3ae3b31829d3844e181bf2d45ef558cc6c0022
Rahul Parkar <rahul.a.parkar@gmail.com>
2021-03-14 18:51:10 +0000

    Initial commit of bitebot

---
+++ index.js
@@ -0,0 +1,65 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+const AsciiTable = require('ascii-table')
+
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const biters = {}
+
+client.on('message', msg => {
+
+  if (msg.content.startsWith('$bite')) {
+    for (const [key, userMentioned] of msg.mentions.users) {
+      let biterRecord = biters?.[msg.author.username];
+
+      if(!biterRecord) {
+        biterRecord = biters[msg.author.username] = {}
+      }
+
+      let biteeRecord = biterRecord?.[userMentioned.username]
+
+      if(!biteeRecord) {
+        biteeRecord = biterRecord[userMentioned.username] = 0;
+      }
+    
+      biteeRecord = biterRecord[userMentioned.username] += 1;
+      
+      msg.reply(`Bite against <@${userMentioned.id}> recorded, use $table to see the current leaderboard.`, {
+        allowedMentions: {
+          parse: ['users']
+        }
+      });
+    }
+  }
+
+  if(msg.content.startsWith('$table')) {
+    for(const [key, userMentioned] of msg.mentions.users) {
+      const table = new AsciiTable()
+      table.setHeading('Chomper', 'Number of Chomps');
+
+      const biterRecord = biters[userMentioned.username];
+
+      if(biterRecord) {
+        for(const [chomper, count] of Object.entries(biterRecord)) {
+          table.addRow(`@${chomper}`, count)
+        }
+
+        console.log(table.toString())
+
+        msg.reply(`\n\n\`\`\`\n${table.toString()}\`\`\``)
+      }
+      else {
+        msg.reply('No table for that user')
+      }
+
+    }
+
+
+  }
+});
+ 
+client.login(process.env.BOT_TOKEN);
